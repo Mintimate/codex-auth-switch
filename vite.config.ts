009b3b84ade9a244e776738121eb8890f5d55928
Mintimate <mintimate215@gmail.com>
2026-09-02 23:25:57 +0800

    refactor(i18n): 拆分界面翻译资源

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  json: {
+    namedExports: false,
+    stringify: true,
+  },
   clearScreen: false,
   server: {
     port: 1420,
@@ -10,4 +14,3 @@ export default defineConfig({
   },
   envPrefix: ["VITE_", "TAURI_ENV_"],
 });
-
